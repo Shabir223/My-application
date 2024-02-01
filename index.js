07f0ce2086ef16860b07c8e4eb860a3b684db403
Shabir223 <155006766+Shabir223@users.noreply.github.com>
2024-02-01 14:56:40 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+const http = require("http");
+const fs = require("fs");
+const port = process.env.Port;
+const handle = (fileName, statusCode, req, res) =>{
+    fs.readFile(fileName,"utf-8",(eer,data) =>{
+        if(eer){
+            console.log(eer)
+        }else{
+            res.writeHead(200,{
+                "content-type":"Text/html"});
+                res.write(data);
+                res.end()
+        };
+      });
+};
+
+
+const server = http.createServer((req,res)=>{
+    if(req.url === "/"){
+        handle("index.html",200,req,res);
+    }else if(req.url === "/about"){
+        handle("about.html",200,req,res);
+    }else if(req.url === "/contact"){
+        handle("contact.html",200,req,res);
+    }else{
+        handle("404.html",404 ,req, res)
+    }
+
+    
+});
+        
+
+server.listen(port, ()=>{
+    console.log(`Server is running`)
+});
